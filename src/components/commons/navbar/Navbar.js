863c34240c52186b4be3162d14f92fe81dfba852
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2022-04-12 21:37:45 -0400

    add icon

--- src/components/commons/navbar/Navbar.js
+++ src/components/commons/navbar/Navbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {  Link } from "react-router-dom";
+import logo from "./../../../assets/img/logo.jpeg";
 import './Navbar.css';
 const Navbar= () =>{
   return (
     <div className="Navbar-container">
       <div className="Navbar-icon">
         <Link to="/">
-          <h1>
-          hello logo
-          </h1> 
+          <img src={logo}></img>
         </Link>
       </div>
     </div>
