1bb5dcd05dca33c7a906b207af3690ae81d1efab
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2022-04-13 14:05:40 -0400

    change align

--- src/components/commons/navbar/Navbar.js
+++ src/components/commons/navbar/Navbar.js
@@ -7,15 +7,10 @@ const Navbar= () =>{
     <div className="Navbar-container">
       <div className="Navbar-icon">
         <Link to="/">
-          other 
-        </Link>
-      </div>
-      <div className="Navbar-picture">
-        <Link to="/">
           <img src={logo}></img>
         </Link>
       </div>
-      <div className="Navbar-icon">
+      <div className="Navbar-options">
         <Link to="/">
           galley
         </Link>
