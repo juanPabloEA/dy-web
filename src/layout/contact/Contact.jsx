1038070df13cb828815524ef598b754eba3054ee
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2022-04-26 13:56:49 -0400

    change background

--- src/layout/contact/Contact.jsx
+++ src/layout/contact/Contact.jsx
@@ -14,7 +14,6 @@ const Contact = () => {
                 <button type="button" className="btn-facebook"><FaFacebook /> Facebook</button>
             </div>
             <div className="item-a">
-               
                 <button type="button" className="btn-instagram"> <FaInstagram /> instagram</button>
             </div>
         </>
