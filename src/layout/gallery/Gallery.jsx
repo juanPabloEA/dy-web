ee9adb0bb007b7e98fee4d0bd9b4023ac3ea1b6d
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2022-05-17 14:03:30 -0400

    refactor cards

--- src/layout/gallery/Gallery.jsx
+++ src/layout/gallery/Gallery.jsx
@@ -6,27 +6,20 @@ import figuresPicture from '../../assets/img/categories/figures.jpeg';
 import eventsPicture from '../../assets/img/categories/eventos_03.jpeg';
 import varPicture from '../../assets/img/categories/traditional.jpeg';
 
-window.onbeforeunload = function(e){
-    console.log('bye bye');
-    document.getElementsByClassName('myDiv').item(0).className = 'animate__bounceOut';
-}
-
 const Gallery = () => {
     return (
         <>
             <div className="Gallery">
-                <div className="traditional animate__animated animate__zoomIn">
+                <div className="card animate__animated animate__zoomIn">
                     <Product picture={traditionalPicture} title="Clasicas" />
                 </div>
-                <div className="fondant animate__animated animate__zoomIn">
+                <div className="card animate__animated animate__zoomIn">
                     <Product picture={figuresPicture} title="Figuras de fondant" />
                 </div>
-            </div>
-            <div className="Gallery">
-                <div className="traditional animate__animated animate__zoomIn">
+                <div className="card animate__animated animate__zoomIn">
                     <Product picture={eventsPicture} title="Eventos" />
                 </div>
-                <div className="fondant animate__animated animate__zoomIn">
+                <div className="card animate__animated animate__zoomIn">
                     <Product picture={varPicture} title="A tú gusto" />
                 </div>
             </div>
