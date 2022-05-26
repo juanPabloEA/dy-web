1938d2ea4aa3b221d4802887e0e09e51b4eb7487
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2022-05-26 14:06:22 -0400

    add input select and use hooks

--- src/layout/shop/content/options/Options.jsx
+++ src/layout/shop/content/options/Options.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import './Options.css'
-
+import InputSelect from "../../../../components/utils/inp-select/InpSelect.jsx"
 export default class Options extends React.Component {
     constructor(props) {
         super(props);
-        this.state = props.opt;
+        console.log(props)
+        this.state = {
+            opt: props.opt
+        }
     }
-    render(){
-        return (<div>hello options {this.props.opt.id}</div>)
+    getOptionList() {
+        return this.state?.opt?.content?.options?.map(option => {
+            return <InputSelect
+                key={option.id}
+                name={option.title} 
+                value={option.id}
+                />
+        });
+
+    }
+
+    render() {
+        return (<div>{this.getOptionList()}</div>)
     }
 }
