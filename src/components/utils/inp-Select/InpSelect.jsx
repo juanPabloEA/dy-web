9e187d184042931544c222104708c4798deb9630
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2022-05-27 17:22:14 -0400

    try to change checkbox state

--- src/components/utils/inp-Select/InpSelect.jsx
+++ src/components/utils/inp-Select/InpSelect.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import './InpSelect.css';
 
-const InpSelect = ({name, value}) => {
-    return (
+export default class InpSelect extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+        this.state = {
+            name: props.name,
+            checked: false,
+            value: props.value
+        }
+    }
+    handleChange(event) {
+        this.state.setChecked(!event.target.checked);
+        console.log(event.target);
+        console.log(event.target.checked);
+    }
+
+    render() {
+         return (
         <div className="InpSelect">
-            {name}
-        <input type="checkbox" name={name} value={value}
+            {this.state.name}
+        <input type="checkbox" id={this.state.value} name={this.state.name}
+            value={this.state.value} 
             />
             <span className="checkmark"></span>
         </div>
     );
-}
+    }
 
-export default InpSelect;
+}   
+
+
+
