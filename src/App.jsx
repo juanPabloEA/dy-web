02672d6bf90fc5e3511e11f6654f109b6863d58e
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2022-06-13 18:32:37 -0400

    fix imports in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Navbar from "./components/commons/navbar/Navbar";
 import Sidebar from "./components/commons/sidebar/Sidebar";
-import Contact from "./layout/contact/Contact";
-import Gallery from "./layout/gallery/Gallery";
+import Contact from "./pages/contact/Contact";
+import Gallery from "./pages/gallery/Gallery";
 import Footer from "./components/commons/footer/Footer";
 import {Routes, Route } from "react-router-dom";
-import Home from './layout/home/Home';
-import Shop from "./layout/shop/Shop";
-import ErrorBoundary from "./layout/error/ErrorBoundary.jsx" 
+import Home from './pages/home/Home';
+import Shop from "./pages/shop/Shop";
+import ErrorBoundary from "./pages/error/ErrorBoundary.jsx" 
 import './App.css';
 
 function App() {
