5c431260646321d51863a7ef754256a4057ffeb7
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2022-06-13 22:09:17 -0400

    add disabled param to limit select group

--- src/components/utils/inp-Select/InpSelect.jsx
+++ src/components/utils/inp-Select/InpSelect.jsx
@@ -8,17 +8,15 @@ export default class InpSelect extends React.Component {
 
     render() {
          return ( 
-             <label className="InpSelect" onClick={this.props?.setCkecked(this.props)} >
-            {this.props.name}
-        <input type="checkbox" id={this.props.value} name={this.props.name}
-            value={this.props.value} defaultChecked={this.props.checked}
-            />
-            <span className="checkmark"></span>
-        </label>
+             <label className={`InpSelect ${
+      true ? "disabled" : ""
+    }`} > 
+                {this.props.name}
+                <input type="checkbox" id={this.props.value} name={this.props.name}
+                    value={this.props.value} onChange={this.props?.setCkecked}/>
+                <span className="checkmark"></span>
+            </label>
     );
     }
 
-}   
-
-
-
+}
